8d04e1ee1bac63f2a9d38209983826cb62de279c
CHRIS CHENG <CHRISCHENGZH@GMAIL.COM>
2022-06-19 17:55:58 +0800

    Initial Commit

---
+++ AR Action 0.3/AR_Action_0_3App.swift
@@ -0,0 +1,17 @@
+//
+//  AR_Action_0_3App.swift
+//  AR Action 0.3
+//
+//  Created by Chris Cheng on 2022/6/19.
+//
+
+import SwiftUI
+
+@main
+struct AR_Action_0_3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
